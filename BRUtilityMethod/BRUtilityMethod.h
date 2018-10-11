6eb5befaea446816a6a1210aec16420d65a7b969
Balaji <lts-balaji.ramakrish@rakuten-life.co.jp>
2018-10-11 13:14:38 +0530

    Initial Commit

---
+++ BRUtilityMethod/BRUtilityMethod.h
@@ -0,0 +1,19 @@
+//
+//  BRUtilityMethod.h
+//  BRUtilityMethod
+//
+//  Created by srm on 11/10/18.
+//  Copyright © 2018 rijalab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BRUtilityMethod.
+FOUNDATION_EXPORT double BRUtilityMethodVersionNumber;
+
+//! Project version string for BRUtilityMethod.
+FOUNDATION_EXPORT const unsigned char BRUtilityMethodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BRUtilityMethod/PublicHeader.h>
+
+
